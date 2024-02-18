e1f16a956568fd6dd2d62f92712a01dac617b8d3
ZeinDeRikko <129055694+ZeinDeRikko@users.noreply.github.com>
2024-02-18 12:22:59 +0200

    Update form.py

--- modules/form.py
+++ modules/form.py
@@ -255,5 +255,6 @@ class Form:
                 "http": "http://" + proxy,
                 "https": "http://" + proxy,
             })
-
+        
+        session.headers.update({"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"})
         return session
